18b0392e713fc0e3df7b86b370a1d8f96bc7c840
woojang <woojang1230@gmail.com>
2024-06-07 18:29:14 +0900

    2.3.1: UserDetailService, PasswordEncoder 재정의

---
+++ ch02_ex2/src/main/java/com/woojang/service/ch02ex2/config/ProjectConfig.java
@@ -0,0 +1,28 @@
+package com.woojang.service.ch02ex2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        final var userDetailService = new InMemoryUserDetailsManager();
+        final var user = User.withUsername("Jang")
+                .password("12345")
+                .authorities("read")
+                .build();
+        userDetailService.createUser(user);
+        return userDetailService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
