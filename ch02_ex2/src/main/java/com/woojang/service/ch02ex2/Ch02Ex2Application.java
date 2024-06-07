129b627dbf8c2ea6cd2e5ee94ff534bb886884b3
woojang <woojang1230@gmail.com>
2024-06-07 17:09:52 +0900

    2.3.1: 서브모듈 추가

---
+++ ch02_ex2/src/main/java/com/woojang/service/ch02ex2/Ch02Ex2Application.java
@@ -0,0 +1,13 @@
+package com.woojang.service.ch02ex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch02Ex2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch02Ex2Application.class, args);
+    }
+
+}
---
+++ ch02_ex2/src/test/java/com/woojang/service/ch02_ex2/Ch02Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woojang.service.ch02_ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch02Ex2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
