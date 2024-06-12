cf529940a6776c6f8e5907cfc3ac452fb5b4179e
woojang <woojang1230@gmail.com>
2024-06-12 16:38:37 +0900

    5.3.1: 예제코드 추가

---
+++ ch05_ex3/src/main/java/com/woojang/service/ch05ex3/Ch05Ex3Application.java
@@ -0,0 +1,13 @@
+package com.woojang.service.ch05ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch05Ex3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch05Ex3Application.class, args);
+    }
+
+}
