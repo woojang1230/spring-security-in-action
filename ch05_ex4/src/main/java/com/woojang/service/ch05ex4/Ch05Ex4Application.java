0c5ebe841af105f3d46992d8fd60d203ac9c35e5
woojang <woojang1230@gmail.com>
2024-06-12 18:08:32 +0900

    5.3.2: 로그인 화면 제공 인증 구현 예제 샘플코드 추가

---
+++ ch05_ex4/src/main/java/com/woojang/service/ch05ex4/Ch05Ex4Application.java
@@ -0,0 +1,13 @@
+package com.woojang.service.ch05ex4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch05Ex4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch05Ex4Application.class, args);
+    }
+
+}
