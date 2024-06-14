5d8f0178afd30b56ab7c1e7ff1b64d7a8b165cca
woojang <woojang1230@gmail.com>
2024-06-14 15:26:52 +0900

    7.1: 권한과 역할에 따라 접근 제한(3)

---
+++ ch07_ex3/src/main/java/com/woojang/service/ch07ex3/Ch07Ex3Application.java
@@ -0,0 +1,13 @@
+package com.woojang.service.ch07ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch07Ex3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch07Ex3Application.class, args);
+    }
+
+}
