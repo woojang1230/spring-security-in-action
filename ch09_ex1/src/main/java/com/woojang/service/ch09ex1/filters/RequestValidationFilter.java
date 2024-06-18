aa392db63afe0debf9e004e3c6d921b552f90586
woojang1230 <woojang1230@gmail.com>
2024-06-18 11:39:56 +0900

    9.2-3: 체인에서 기존 필터 앞/뒤에 필터 추가

---
+++ ch09_ex1/src/main/java/com/woojang/service/ch09ex1/filters/RequestValidationFilter.java
@@ -0,0 +1,28 @@
+package com.woojang.service.ch09ex1.filters;
+
+import java.io.IOException;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class RequestValidationFilter implements Filter {
+    @Override
+    public void doFilter(
+            final ServletRequest servletRequest,
+            final ServletResponse servletResponse,
+            final FilterChain filterChain
+    ) throws IOException, ServletException {
+        final HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        final HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        final String requestId = httpRequest.getHeader("Request-Id");
+        if (requestId == null || requestId.isBlank()) {
+            httpResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
