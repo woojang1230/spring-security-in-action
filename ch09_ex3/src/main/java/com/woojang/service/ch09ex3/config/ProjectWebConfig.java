7bd46d348cecb9d46bb7d7c380747a2b3f094ca2
woojang1230 <woojang1230@gmail.com>
2024-06-18 11:24:31 +0900

    9.5: 스프링 시큐리티가 제공하는 필터 구현

---
+++ ch09_ex3/src/main/java/com/woojang/service/ch09ex3/config/ProjectWebConfig.java
@@ -0,0 +1,25 @@
+package com.woojang.service.ch09ex3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.woojang.service.ch09ex3.filters.AuthenticationLoggingFilter;
+import com.woojang.service.ch09ex3.filters.RequestValidationFilter;
+
+@Configuration
+public class ProjectWebConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.addFilterBefore(
+                        new RequestValidationFilter(),
+                        BasicAuthenticationFilter.class)
+                .addFilterAfter(
+                        new AuthenticationLoggingFilter(),
+                        BasicAuthenticationFilter.class)
+                .authorizeRequests()
+                .anyRequest()
+                .permitAll();
+    }
+}
